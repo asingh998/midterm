ae7e7d1ac46cc5a4d64fdce0014a3290746963e3
Arshdeep Singh <asingh69@mail.greenriver.edu>
2020-05-20 12:32:50 -0400

    added config files

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once("vendor/autoload.php");
+
+//Instantiate the F3 Base class
+$f3 = Base::instance();
+
+//Default route
+$f3->route('GET /', function() {
+    echo '<h1>Midterm Survey</h1>';
+    echo '<a href="">Take my midterm survey</a>';
+
+    //$views = new Template();
+    //echo $views->render('views/home.html');
+});
+
+//Run F3
+$f3->run();
